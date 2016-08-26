53b6af541cac095c15b0bd8223ca411647eb96c7
Iason Sarantopoulos <iason@sarantop.com>
2016-08-27 02:10:58 +0300

    Add MongoDB image

---
+++ mongo/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Build the docker image
+docker build -t iason/mongodb .
---
+++ mongo/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker run -ti \
+       -p 27017:27017 \
+       iason/mongodb
